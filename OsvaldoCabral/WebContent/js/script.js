c71592b43ee3d5827a5aa513c288d6c71eb78b40
osvaldocabral <osvaldo_cabral@outlook.com.br>
2017-08-01 08:39:58 -0300

    Commit inicial

---
+++ OsvaldoCabral/WebContent/js/script.js
@@ -0,0 +1,13 @@
+
+function abrirFecharMenu() {
+	var btnMenu = "confinado";
+	var btn = document.getElementById("bs-example-navbar-collapse-1");
+	
+	if(btnMenu === "expandir"){
+		btn.className = "navbar-collapse collapse in";
+		btnMenu = "confinado";
+	} else {
+		btn.className = "navbar-collapse collapse";
+		btnMenu = "expandir";
+	}
+}
