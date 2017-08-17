548207f0353f79fec684b8a4cbcac4182014db36
Osvaldo Cabral <osvaldo_cabral@outlook.com.br>
2017-08-17 00:00:15 -0300

    Ultima atualização

--- js/script.js
+++ js/script.js
@@ -2,12 +2,22 @@
 function abrirFecharMenu() {
 	var btnMenu = "confinado";
 	var btn = document.getElementById("bs-example-navbar-collapse-1");
-	
-	if(btnMenu === "expandir"){
+
+	if (btnMenu === "expandir") {
 		btn.className = "navbar-collapse collapse in";
 		btnMenu = "confinado";
 	} else {
 		btn.className = "navbar-collapse collapse";
 		btnMenu = "expandir";
 	}
+}
+
+function redirecionar() {
+    $("li a").on('click', function() {
+        $("body").animate({
+            scrollTop: $($(this).attr('href')).offset().top - 120
+		}, 400);
+        abrirFecharMenu();
+        return false;
+    });
 }
