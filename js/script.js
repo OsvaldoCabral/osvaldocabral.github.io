4dea338ea4f6784bb9f922f18cffcbc21c658b6c
Osvaldo Cabral <osvaldo_cabral@outlook.com.br>
2018-02-03 23:16:26 -0200

    Corrigindo menu para mibiles

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 
 function abrirFecharMenu() {
-	var btnMenu = "confinado";
-	var btn = document.getElementById("bs-example-navbar-collapse-1");
-
-	if (btnMenu === "expandir") {
-		btn.className = "navbar-collapse collapse in";
-		btnMenu = "confinado";
-	} else {
-		btn.className = "navbar-collapse collapse";
-		btnMenu = "expandir";
+	
+	try { 
+		var btn = document.getElementById("bs-example-navbar-collapse-1");
+	
+		if (btn.className === "navbar-collapse collapse") {
+			btn.className = "navbar-collapse collapse in";
+		} else {
+			btn.className = "navbar-collapse collapse";
+			return;
+		}
+	
+	} catch(e) {
+		
 	}
 }
